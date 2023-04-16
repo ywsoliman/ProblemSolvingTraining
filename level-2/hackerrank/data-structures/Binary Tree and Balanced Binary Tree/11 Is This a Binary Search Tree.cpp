f47ba1355fb93cf738baa5bbddfe12ed8b03706d
Youssef W. Soliman <ywsoliman@gmail.com>
2023-04-16 16:37:07 +0200

    Update 11 Is This a Binary Search Tree.cpp

--- level-2/hackerrank/data-structures/Binary Tree and Balanced Binary Tree/11 Is This a Binary Search Tree.cpp
+++ level-2/hackerrank/data-structures/Binary Tree and Balanced Binary Tree/11 Is This a Binary Search Tree.cpp
@@ -1,9 +1,9 @@
 bool checkBST(Node* root) {
-    return checkBST(root, 0, INT16_MAX);
+    return checkBST(root, INT16_MIN, INT16_MAX);
 }
 bool checkBST(Node* root, int minVal, int maxVal) {
     if (root == nullptr)
         return true;
-    if (root->data >= maxVal || root->data <= minVal) return false;
-    return checkBST(root->left, minVal, root->data) && checkBST(root->right, root->data, maxVal);
+    if (root->data > maxVal || root->data < minVal) return false;
+    return checkBST(root->left, minVal, root->data - 1) && checkBST(root->right, root->data + 1, maxVal);
 }
